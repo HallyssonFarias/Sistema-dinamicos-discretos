a201d4895688ca7906c714380c70f64bfd3240d0
HallyssonFarias <87992633+HallyssonFarias@users.noreply.github.com>
2023-06-04 13:47:13 -0300

    Iterações com a logística.py

--- Iterações com a logística.py
+++ Iterações com a logística.py
@@ -1,46 +1,42 @@
-# OBS: A lista não é usada no código é apenas para facilitar quando quiser visualizar mais facilmente,
-# basta somente apagar '#' da frente da última linha
+# OBS: Recomenda-se usar, na maioria das vezes, a população inicial igual à 0
 
 # Printa para mostrar a função logística
 print("A funcao é : f(x)= r*x(1-x)+ xo")
 
 # Escolhe as variáveis
-r = float(input("escolha o r: "))
-x = float(input("escolha o x: "))
-xo = float(input("(populacao incial): "))  # Recomenda-se usar, na maioria das vezes, a população inicial igual à 0
+r = float(input("Escolha o valor de r: "))
+x = float(input("Escolha o valor de x: "))
+xo = float(input("Escolha o valor da Populacao inicial(xo): "))
 
 # Escolhe a quantidade de casais decimais máximo de 16
-decimal = int(input("escolha a quantidade de casas decimais: "))
+decimal = int(input("Escolha a quantidade de casas decimais: "))
 
 # Escolhe a quantidade de iterações e também quais iterações que serão printadas
-opcao = int(input("opcao 1: 1 até n \nopcao 2: de a até b \nescolha sua opcao: "))
+opcao = int(input("Opcao 1: 1 até n \nOpcao 2: de a até b \nescolha sua opcao: "))
 
-# Parte do código que itera a logística
-lista = []
-c = 0
-if opcao == 1:
-    # Essa parte é para a 1 opção de 1 até n
-    k = int(input("de 1 até quanto? "))
-    while c < k:
+# Funcao que itera a logística
+
+
+def iteracao(r, x, xo, decimal, opcao, k, a):
+    for i in range(k+1):
         y = r*x*(1-x)+xo
         x = y
         xo = 0
         y = round(y, decimal)
-        print(y)
-        c = c + 1
-        lista.append(x)
-else:
-    # Essa parte é para a segunda opção de a até b
-    a = int(input("de: "))
-    b = int(input("até: "))
-    while c < b:
-        y = r*x*(1-x)+xo
-        x = y
-        xo = 0
-        c = c + 1
-        lista.append(x)
-        if c >= a:
-            y = round(y, decimal)
-            print(y)
+        if opcao == 1 and i < k:
+            print(f"{i}º iteração: \n {y}")
+        elif opcao == 2 and i >= a and i <= k:
+            print(f"{i}º iteração: \n {y}")
 
-# print(lista)
+
+# opcoes que chamam a funcao
+if opcao == 1:
+    k = int(input("De 1 até quanto? "))
+    iteracao(r, x, xo, decimal, opcao, k, 0)
+
+elif opcao == 2:
+    # Essa parte é para a segunda opcao de a até b
+    a = int(input("de: "))
+    k = int(input("até: "))
+    iteracao(r, x, xo, decimal, opcao, k, a)
+
